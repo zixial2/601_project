bab8430f610bbe7e9349cfc4bebace9beb0cd16f
zixia luan <zixial@andrew.cmu.edu>
2020-11-18 20:16:09 -0600

    add needed packages

--- 601_shiny/app.R
+++ 601_shiny/app.R
@@ -1,7 +1,38 @@
 
 # shiny design
 
-library(shiny)
+
+
+#install.packages("ggplot2")
+#install.packages("leaflet")
+#install.packages("leaflet.extras")
+#install.packages("dplyr")
+#install.packages("plotly")
+#install.packages("shinythemes")
+#install.packages("tmap")
+#install.packages("shinyWidgets")
+
+
+#pkg_list = c("ggplot2", "leaflet", "leaflet.extras", "dplyr", "plotly", "shinythemes", "tmap", "stringr", "shinyWidgets", "rbokeh")
+#mia_pkgs = pkg_list[!(pkg_list %in% installed.packages()[,"Package"])]
+#if(length(mia_pkgs) > 0) install.packages(mia_pkgs)
+#deplloaded_pkgs = lapply(pkg_list, require, character.only=TRUE)
+
+
+
+#library(tmap)
+#library(sf)
+require(plotly)
+require(dplyr)
+require(leaflet.extras)
+require(leaflet)
+require(ggplot2)
+require(shiny)
+require(shinydashboard)
+require(shinythemes)
+require(stringr)
+require("rbokeh")
+require(shinyWidgets)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
