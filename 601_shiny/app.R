183ec243d246fbe41f446fd9c7dd13a2c08c96b1
zixia luan <zixial@andrew.cmu.edu>
2020-11-19 21:00:02 -0600

    add plot to second tab

--- 601_shiny/app.R
+++ 601_shiny/app.R
@@ -33,8 +33,23 @@ require(shinythemes)
 require(stringr)
 require("rbokeh")
 require(shinyWidgets)
-
+library("jsonlite")
 # Define UI for application that draws a histogram
+
+
+# read real time data:
+
+
+real = fromJSON("https://gbfs.capitalbikeshare.com/gbfs/fr/station_status.json")
+real = as.data.frame(real)[,c(-14,-15)]
+a = fromJSON("https://gbfs.capitalbikeshare.com/gbfs/fr/station_information.json")$data
+a = as.data.frame(a)
+colnames(a) = str_extract(colnames(a), '\\b\\w+$')
+colnames(real) = str_extract(colnames(real), '\\b\\w+$')
+a$station_id = as.character(a$station_id)
+real$station_id = as.character(real$station_id)
+real_data = na.omit(left_join(real,a,"station_id"))
+
 ui <- fluidPage(
   theme = shinytheme("cosmo"),
   navbarPage(
@@ -42,20 +57,27 @@ ui <- fluidPage(
     title="Bike Sharing in Washington DC",
     tabPanel("About", sidebarPanel(h3("Introduction:"), textOutput("intro")),
              "Designed by: Risberg"),
-    
-    tabPanel("Explore all Stations"),
+    # second tab
+    tabPanel("Explore all Stations",
+             sidebarPanel("Choose Interested Information:",
+                          pickerInput("picker", 
+                                      label = "Info To Show:", 
+                                      choices = c("Station Name"="name", "Available Docks"="num_docks_available", "Disabled Bikes"="num_bikes_disabled", "Station Status"="station_status","Station Capacity"="capacity", "Has Kiosk" = "has_kiosk","Rental Method" = "rental_methods"), 
+                                      options = list(`actions-box` = T, size = 15,`selected-text-format` = "count > 3"), 
+                                      multiple = TRUE)),
+             mainPanel(rbokehOutput("graph",width=800,height=550))),
     
     tabPanel("Predict Bikes", 
              sidebarPanel(
                # Add user inputs here
                
                actionButton(inputId = "action", label = "Predict!")),
-
+             
              
              mainPanel(h3("Predicted Number of Bikes"),
-                        verbatimTextOutput("bikes"),
-                        h3("Map showing nearest 3 stations"),
-                        verbatimTextOutput("Map1"))),
+                       verbatimTextOutput("bikes"),
+                       h3("Map showing nearest 3 stations"),
+                       verbatimTextOutput("Map1"))),
     
     tabPanel("Potential Rush Hours"),
     tabPanel("Heatmap",
@@ -72,7 +94,23 @@ ui <- fluidPage(
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-
+  # first tab
+  output$intro= renderText(
+    "Our topic is about: ")
+  
+  
+  # second tab: 
+  output$graph=renderRbokeh({
+    suppressWarnings(figure(width = 800, height = 450, padding_factor = 0,
+                            xlim = range(real_data$lon),ylim = range(real_data$lat)) %>%
+                       ly_map("county", "washington",col = 'indigo') %>%
+                       ly_points(lon, lat, data =real_data, size = 6,
+                                 hover =input$picker)%>%
+                       x_axis(visible = T) %>%
+                       y_axis(visible = T))
+  })
+  
+  
 }
 
 # Run the application 
